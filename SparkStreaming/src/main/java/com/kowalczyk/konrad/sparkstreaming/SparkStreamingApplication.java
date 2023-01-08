f6a14a1f63417945c3c27def183ea9190fc750e6
Konrad <konrad.kowalczyk.98@gmail.com>
2023-01-08 17:33:36 +0100

    add Consumer, Producer and SparkStreaming app to project with base conf

---
+++ SparkStreaming/src/main/java/com/kowalczyk/konrad/sparkstreaming/SparkStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.kowalczyk.konrad.sparkstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkStreamingApplication.class, args);
+    }
+
+}
---
+++ SparkStreaming/src/test/java/com/kowalczyk/konrad/sparkstreaming/SparkStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kowalczyk.konrad.sparkstreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
