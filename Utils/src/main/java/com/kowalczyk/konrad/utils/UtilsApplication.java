922907917a3b5ea007d21e967193730bc4d5773d
Konrad <konrad.kowalczyk.98@gmail.com>
2023-01-08 14:53:56 +0100

    add multi module pom and add project Utils

---
+++ Utils/src/main/java/com/kowalczyk/konrad/utils/UtilsApplication.java
@@ -0,0 +1,14 @@
+package com.kowalczyk.konrad.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsApplication.class, args);
+    }
+
+}
