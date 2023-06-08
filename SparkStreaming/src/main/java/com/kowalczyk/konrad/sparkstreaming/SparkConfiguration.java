b39a73c7f3ece78737782b670f0c37d16362df6c
Konrad <konrad.kowalczyk.98@gmail.com>
2023-06-08 23:59:33 +0200

    test-3 - add solution to count average to spark structured streaming. Unfortunately, nothing related to grouping, aggregation and other solutions worked.

--- SparkStreaming/src/main/java/com/kowalczyk/konrad/sparkstreaming/SparkConfiguration.java
+++ SparkStreaming/src/main/java/com/kowalczyk/konrad/sparkstreaming/SparkConfiguration.java
@@ -2,9 +2,12 @@ package com.kowalczyk.konrad.sparkstreaming;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
 import org.apache.spark.sql.types.StructType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +19,9 @@ import static org.apache.spark.sql.types.DataTypes.*;
 
 @Configuration
 public class SparkConfiguration {
+
+    public static double sum = 0;
+    public static long count = 0;
 
     @Bean
     public SparkConf sparkConf() {
@@ -51,8 +57,8 @@ public class SparkConfiguration {
                 .add("indicator", StringType)
                 .add("stationCode", StringType)
                 .add("timestampSend", LongType)
-                .add("timestampStream", LongType)
-                .add("timestampConsumer", LongType);
+                .add("timestampConsumer", LongType)
+                .add("averagingValue", DoubleType);
     }
 
     @Bean
@@ -69,9 +75,33 @@ public class SparkConfiguration {
                 .select(from_json(col("value"), getSchema()).as("data"), col("key").as("key"))
                 .selectExpr("key", "data.*");
 
-        Dataset<Row> filteredDf = df.filter(col("value").gt(0));
 
-        filteredDf.selectExpr("CAST(key AS STRING)", "to_json(struct(*)) AS value")
+        Dataset<Row> positionCode = df.filter(col("value").gt(0))
+                .map((MapFunction<Row, Row>) row -> {
+                    Double value = (Double) row.get(2);
+                    sum += value;
+                    count++;
+                    double average = sum / count;
+
+                    Row updatedRow = RowFactory.create(
+                            null,
+                            (String) row.get(1),
+                            (Double) row.get(2),
+                            (String) row.get(3),
+                            (String) row.get(4),
+                            (String) row.get(5),
+                            (String) row.get(6),
+                            (String) row.get(7),
+                            (Long) row.get(8),
+                            (Long) row.get(9),
+                            (Double) average
+                    );
+
+                    return updatedRow;
+                }, RowEncoder.apply(df.schema()));
+
+
+        positionCode.selectExpr("CAST(key AS STRING)", "to_json(struct(*)) AS value")
                 .writeStream()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "localhost:9092")
